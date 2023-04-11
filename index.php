0a3eaed523ee981d361a99ce33733e050c153498
albertogioia93 <albertogioia@hotmail.it>
2023-04-11 18:28:18 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Strong Password Generator</title>
+</head>
+<body>
+    <form action="index.php" method="POST">
+        <div>
+            <label for="exampleInputPassword1" class="form-label">password</label>
+            <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <button type="submit">submit</button>
+    </form>
+
+</body>
+</html>
